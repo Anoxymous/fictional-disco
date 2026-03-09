81b1b6bcadb9ab173c07300cd41e5bdae9a0d2d3
Anoxymous <timy.cross@gmail.com>
2026-03-09 11:27:25 +1000

    initial example

---
+++ app/src/main/java/com/stealthsummit/studios/fictionaldisco/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.stealthsummit.studios.fictionaldisco
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
+
+        // Enable JS + local storage
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.allowFileAccess = true
+        settings.allowFileAccessFromFileURLs = true
+        settings.allowUniversalAccessFromFileURLs = true
+
+        // Keep navigation inside the WebView
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        // Load local HTML app
+        webView.loadUrl("file:///android_asset/www/index.html")
+    }
+
+    override fun onBackPressed() {
+        val webView = findViewById<WebView>(R.id.webview)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
